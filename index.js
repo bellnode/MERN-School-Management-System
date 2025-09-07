6d4215c841ea970bf7e0427f9f67b1693ac7c72d
Bellnode Technology <bellnodetechnology@gmail.com>
2025-09-07 11:31:23 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import store from './redux/store';
-import { Provider } from 'react-redux';
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+const app = express()
+const Routes = require("./routes/route.js")
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-)
+const PORT = process.env.PORT || 5000
+
+dotenv.config();
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
